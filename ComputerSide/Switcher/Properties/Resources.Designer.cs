6ec4e255d3cb65bfa148f61da22c12027ae9dfef
Randall Borck <rgborck@hotmail.com>
2014-12-22 12:52:48 -0500

    Removed nonexistant resource.

--- ComputerSide/Switcher/Properties/Resources.Designer.cs
+++ ComputerSide/Switcher/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Switcher.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Serious_Froggy {
-            get {
-                object obj = ResourceManager.GetObject("Serious Froggy", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
